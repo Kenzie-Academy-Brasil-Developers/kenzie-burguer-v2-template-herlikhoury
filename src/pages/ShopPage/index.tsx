b05fa367eb5ab8f6f37a88750c6719068f4a6dec
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-03-07 17:58:26 -0300

    Feature: load product list

--- src/pages/ShopPage/index.tsx
+++ src/pages/ShopPage/index.tsx
@@ -1,20 +1,31 @@
-import { StyledShopPage } from './style';
+import { useContext, useEffect } from 'react';
 import CartModal from '../../components/CartModal';
 import Header from '../../components/Header';
 import ProductList from '../../components/ProductList';
-
+import { ProductsCartContext } from '../../contexts/ProductsCartContext';
 import { StyledContainer } from '../../styles/grid';
+import { StyledShopPage } from './style';
 
-const ShopPage = () => (
-  <StyledShopPage>
-    <CartModal />
-    <Header />
-    <main>
-      <StyledContainer containerWidth={1300}>
-        <ProductList />
-      </StyledContainer>
-    </main>
-  </StyledShopPage>
-);
+const ShopPage = () => {
+  const { productsList, loadProductsList } = useContext(ProductsCartContext);
+
+  useEffect(() => {
+    loadProductsList();
+    console.log('use effect');
+  }, []);
+
+  /* console.log(productsList); */
+  return (
+    <StyledShopPage>
+      <CartModal />
+      <Header />
+      <main>
+        <StyledContainer containerWidth={1300}>
+          <ProductList />
+        </StyledContainer>
+      </main>
+    </StyledShopPage>
+  );
+};
 
 export default ShopPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import { UserProvider } from './contexts/UserContext';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ShopPage from './pages/ShopPage';
